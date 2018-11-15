33cab7c0719b2aac95880169d7b73bd1f2a35eb7
devil1165 <devkant@getambee.com>
2018-11-15 20:46:09 +0530

    array method added to reading post api

--- api/routes/reading.js
+++ api/routes/reading.js
@@ -6,33 +6,36 @@ const Reading = require('../models/readinModel')
 
 // get all points
 router.post('/', (req,res,next) => {
-
-    var obj = {
-        pm25: req.body.pm25,
-        pm1: req.body.pm1,
-        pm10: req.body.pm10,
-        battery: req.body.battery,
-        location: {
-            type:'Point',
-            coordinates: [req.body.lon,req.body.lat],
-        },
-	timestamp:req.body.timestamp,
-        savedAt:Date.now(),
-        user:req.body.email
-    }
-    var reading = new Reading(obj);
-    reading.save()
-    .then((result) => {
-        res.status(200).json({
-            message : 'data stored successfully',
-            // data:result
+    var data = req.body.data;
+    data.map((v,i) => {
+        var obj = {
+            pm25: v.pm25,
+            pm1: v.pm1,
+            pm10: v.pm10,
+            battery: v.battery,
+            location: {
+                type:'Point',
+                coordinates: [v.lng,v.lat],
+            },
+            timestamp:v.timestamp,
+            savedAt:Date.now(),
+            user:v.email
+        }
+        var reading = new Reading(obj);
+        reading.save()
+        .then((result) => {
+            res.status(200).json({
+                message : 'data stored successfully',
+                // data:result
+            })
+            
         })
-        
-    }).catch((err) => {
-        res.status(500).json({
-            error:err
-        })    
-    });
+        .catch((err) => {
+            res.status(500).json({
+                error:err
+            })    
+        });
+    })
 })
 
 
